b8fdd2b469b9dabb97e362b421d305197ac32191
David Alves <david.a0595@gmail.com>
2024-08-21 17:30:12 -0300

    branch para benchmark

--- Program.cs
+++ Program.cs
@@ -1,25 +1,32 @@
-﻿using GeradorParChaves.Services;
+﻿using BenchmarkDotNet.Running;
+using GeradorParChaves.Benchmarks;
+using GeradorParChaves.Services;
 
 class Program
 {
     static void Main(string[] args)
     {
-        var geradorParChaves = new GeradorParChave();
-        var chaveSecreta = ArmazenamentoChave.GerarChaveSecreta();
-        var iv = ArmazenamentoChave.GerarIV();
+        // Para rodar o benchmark, execute o programa com o argumento "benchmark":
+        // dotnet run -c Release -- benchmark 
+        var summary = BenchmarkRunner.Run<CriptografiaBenchmark>();
 
-        var armazenamentoChave = new ArmazenamentoChave(chaveSecreta, iv);
-        var servicoAuditoria = new ServicoAuditoria();
 
-        var (chavePublica, chavePrivada) = geradorParChaves.GerarParChaves();
+        //var geradorParChaves = new GeradorParChave();
+        //var chaveSecreta = ArmazenamentoChave.GerarChaveSecreta();
+        //var iv = ArmazenamentoChave.GerarIV();
 
-        Guid fornecedorId = Guid.NewGuid();
-        armazenamentoChave.ArmazenarChavePrivada(chavePrivada, fornecedorId);
-        servicoAuditoria.AuditarGeracaoChave("admin");
+        //var armazenamentoChave = new ArmazenamentoChave(chaveSecreta, iv);
+        //var servicoAuditoria = new ServicoAuditoria();
 
-        Console.WriteLine("Chave pública gerada: " + chavePublica);
+        //var (chavePublica, chavePrivada) = geradorParChaves.GerarParChaves();
 
-        var (fornecedorIdRecuperado, chavePrivadaRecuperada) = armazenamentoChave.BuscarChavePrivada();
-        Console.WriteLine($"Fornecedor: {fornecedorIdRecuperado}, Chave privada recuperada: {chavePrivadaRecuperada}");
+        //Guid fornecedorId = Guid.NewGuid();
+        //armazenamentoChave.ArmazenarChavePrivada(chavePrivada, fornecedorId);
+        //servicoAuditoria.AuditarGeracaoChave("admin");
+
+        //Console.WriteLine("Chave pública gerada: " + chavePublica);
+
+        //var (fornecedorIdRecuperado, chavePrivadaRecuperada) = armazenamentoChave.BuscarChavePrivada();
+        //Console.WriteLine($"Fornecedor: {fornecedorIdRecuperado}, Chave privada recuperada: {chavePrivadaRecuperada}");
     }
 }
